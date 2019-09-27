1ea3e9587ef8f40de9795fd9697be0701fa6ea5c
Alex Shavalda <cool.sasha1313@gmail.com>
2019-09-27 16:09:17 +0300

    Was completed TodoApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TodoListComponent} from './todo-list/todo-list.component';
+import {TodoHeaderComponent} from './todo-header/todo-header.component';
+import {TodoCreateComponent} from './todo-create/todo-create.component';
+import {
+    MatButtonModule,
+    MatCheckboxModule, MatDatepickerModule, MatFormFieldModule,
+    MatIconModule, MatInputModule,
+    MatPaginatorModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatNativeDateModule
+} from '@angular/material';
+import {MatSortModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TodoListComponent,
+        TodoHeaderComponent,
+        TodoCreateComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatTableModule,
+        MatSortModule,
+        MatTabsModule,
+        HttpClientModule,
+        MatIconModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatPaginatorModule,
+        MatStepperModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/models/todo.ts
@@ -0,0 +1,7 @@
+export interface Todo {
+    id: number;
+    title: string;
+    start: Date;
+    deadline: Date;
+    completed: boolean;
+}
